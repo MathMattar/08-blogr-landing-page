00fe3ca6e8774a20b78f26ac54049012a070bc59
MathMattar <mathmattar@hotmail.com>
2023-02-20 10:23:51 -0300

    Adding rotation action to arrows

--- assets/js/menuSandwich.js
+++ assets/js/menuSandwich.js
@@ -12,12 +12,15 @@ function changeVisibility() {
   iconCloseMenu.classList.toggle("--active");
 }
 
-//Dropdown Mobile
+//Dropdown
 const dropdownLinks = document.querySelectorAll(".dropdown__link");
+const arrow = document.getElementById("arrow");
 
 //Adiciona um evento de clique em cada link para exibir sua respectiva lista
 dropdownLinks.forEach((dropdownLink) => {
   const dropdownList = dropdownLink.nextElementSibling; //Seleciona a lista associada ao link atual
+  const arrowLight = dropdownLink.querySelector(".dropdown__icon"); //Seleciona a seta associada ao link atual
+  const arrowDark = dropdownLink.querySelector(".dropdown__icon--dark"); //Seleciona a seta associada ao link atual
 
   dropdownLink.addEventListener("click", (e) => {
     e.preventDefault();
@@ -27,11 +30,14 @@ dropdownLinks.forEach((dropdownLink) => {
       if (link !== dropdownLink && link.classList.contains("--active")) {
         link.classList.remove("--active");
         link.nextElementSibling.classList.remove("--active");
+        link.querySelector(".dropdown__icon").classList.remove("--rotate");
       }
     });
 
     //Ativa a lista do link atual
     dropdownLink.classList.toggle("--active");
     dropdownList.classList.toggle("--active");
+    arrowLight.classList.toggle("--rotate");
+    arrowDark.classList.toggle("--rotate");
   });
 });
