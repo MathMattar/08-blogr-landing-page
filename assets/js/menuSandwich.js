e88c1717d9b9de5b19f9febb9a27a34c52b65bc2
MathMattar <mathmattar@hotmail.com>
2023-02-21 18:12:34 -0300

    Update menu

--- assets/js/menuSandwich.js
+++ assets/js/menuSandwich.js
@@ -2,30 +2,38 @@ const iconMenu = document.getElementById("icon-menu");
 const iconCloseMenu = document.getElementById("icon-close");
 const navBar = document.getElementById("nav-bar");
 
-//Show menu sandwich
+// Exibir menu sandwich
 iconMenu.addEventListener("click", changeVisibility);
 iconCloseMenu.addEventListener("click", changeVisibility);
 
+// Alterar o icone do menu
 function changeVisibility() {
   navBar.classList.toggle("--active");
   iconMenu.classList.toggle("--hide");
   iconCloseMenu.classList.toggle("--active");
 }
 
-//Dropdown
+// Dropdown
 const dropdownLinks = document.querySelectorAll(".dropdown__link");
 const arrow = document.getElementById("arrow");
 
-//Adiciona um evento de clique em cada link para exibir sua respectiva lista
+function toggleDropdown(dropdownLink, dropdownList, arrowLight, arrowDark) {
+  dropdownLink.classList.toggle("--active");
+  dropdownList.classList.toggle("--active");
+  arrowLight.classList.toggle("--rotate");
+  arrowDark.classList.toggle("--rotate");
+}
+
+// Adiciona um evento de clique em cada link para exibir sua respectiva lista
 dropdownLinks.forEach((dropdownLink) => {
-  const dropdownList = dropdownLink.nextElementSibling; //Seleciona a lista associada ao link atual
-  const arrowLight = dropdownLink.querySelector(".dropdown__icon"); //Seleciona a seta associada ao link atual
-  const arrowDark = dropdownLink.querySelector(".dropdown__icon--dark"); //Seleciona a seta associada ao link atual
+  const dropdownList = dropdownLink.nextElementSibling; // Seleciona a lista associada ao link atual
+  const arrowLight = dropdownLink.querySelector(".dropdown__icon"); // Seleciona a seta associada ao link atual
+  const arrowDark = dropdownLink.querySelector(".dropdown__icon--dark"); // Seleciona a seta associada ao link atual
 
   dropdownLink.addEventListener("click", (e) => {
     e.preventDefault();
 
-    //Percorre todos os links e desativa quallquer lista que esteja ativa
+    // Percorre todos os links e desativa quallquer lista que esteja ativa
     dropdownLinks.forEach((link) => {
       if (link !== dropdownLink && link.classList.contains("--active")) {
         link.classList.remove("--active");
@@ -34,10 +42,33 @@ dropdownLinks.forEach((dropdownLink) => {
       }
     });
 
-    //Ativa a lista do link atual
-    dropdownLink.classList.toggle("--active");
-    dropdownList.classList.toggle("--active");
-    arrowLight.classList.toggle("--rotate");
-    arrowDark.classList.toggle("--rotate");
+    // Ativa a lista do link atual
+    toggleDropdown(dropdownLink, dropdownList, arrowLight, arrowDark);
   });
+});
+
+// Evento de clique no corpo da página para fechar a lista ou o menu
+document.body.addEventListener("click", (event) => {
+  // Verifica onde foi o clique
+  const dropdownList = event.target.closest(".dropdown__list");
+  const dropdownLink = event.target.closest(".dropdown__link");
+
+  if (
+    !dropdownList &&
+    !dropdownLink &&
+    !event.target.closest("#nav-bar") &&
+    !event.target.closest("#icon-menu")
+  ) {
+    dropdownLinks.forEach((link) => {
+      if (link.classList.contains("--active")) {
+        const activeList = link.nextElementSibling;
+        const activeArrowLight = link.querySelector(".dropdown__icon");
+        const activeArrowDark = link.querySelector(".dropdown__icon--dark");
+
+        toggleDropdown(link, activeList, activeArrowLight, activeArrowDark);
+      }
+    });
+
+    changeVisibility();
+  }
 });
